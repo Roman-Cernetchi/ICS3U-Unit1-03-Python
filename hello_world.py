baa02d9c8d7a1abf6f3786598d72c3a9dbed13fb
Ubuntu <ubuntu@ip-172-31-56-74.ec2.internal>
2020-11-20 18:48:20 +0000

    Added proper Format for hello, world

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Roman Cernetchi
+# Created on: November 2020
+# This is a "Hello, World!" program, but it looks nicer
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
